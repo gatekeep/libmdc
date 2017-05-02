4a90f74a3ee9a5b93c451db05f8c072ec59ecc9d
Bryan Biedenkapp <gatekeep@jmp.cx>
2017-05-02 09:45:00 -0400

    initial commit

---
+++ libmdc/libmdc/MDCGenerator.cs
@@ -0,0 +1,247 @@
+﻿#if NAUDIO
+/**
+ * libmdc
+ * INTERNAL/PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Author: Bryan Biedenkapp <gatekeep@jmp.cx>
+ */
+using System;
+
+using NAudio.Wave;
+
+namespace libmdc
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SamplesToMS
+    {
+        /**
+         * Methods
+         */
+        /// <summary>
+        /// (ms) to sample count conversion
+        /// </summary>
+        /// <param name="format">Wave format</param>
+        /// <param name="ms">Number of milliseconds</param>
+        /// <returns>Number of samples</returns>
+        public static int MSToSamples(WaveFormat format, int ms)
+        {
+            return (int)(((long)ms) * format.SampleRate * format.Channels / 1000);
+        }
+
+        /// <summary>
+        /// samples to bytes conversion
+        /// </summary>
+        /// <param name="format">Wave format</param>
+        /// <param name="samples">Number of samples</param>
+        /// <returns>Number of bytes for the number of samples</returns>
+        public static int SamplesToBytes(WaveFormat format, int samples)
+        {
+            return samples * (format.BitsPerSample / 8);
+        }
+
+        /// <summary>
+        /// (ms) to bytes conversion
+        /// </summary>
+        /// <param name="format">Wave format</param>
+        /// <param name="ms">Number of milliseconds</param>
+        /// <returns>Number of bytes for the amount of audio in (ms)</returns>
+        public static int MSToSampleBytes(WaveFormat format, int ms)
+        {
+            return SamplesToBytes(format, MSToSamples(format, ms));
+        }
+    } // public class SamplesToMS
+
+    /// <summary>
+    /// Implements a sample provider that synthesizes audio for MDC1200 packet(s).
+    /// </summary>
+    public class MDCGenerator : ISampleProvider
+    {
+        /**
+         * Fields
+         */
+        private const int PREAMBLE_LEN_MS = 3;
+        private const int PACKET_LEN_MS = 173;
+        private const int SILENCE_MS = 75;
+        private const float GAIN = 0.95f;
+
+        private MDCEncoder encoder;
+        private readonly WaveFormat waveFormat;
+
+        private bool doublePacket = false;
+        private bool mdcGenerated = false;
+
+        /**
+         * Properties
+         */
+        /// <summary>
+        /// The waveformat of this WaveProvider (same as the source)
+        /// </summary>
+        public WaveFormat WaveFormat
+        {
+            get { return waveFormat; }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of preambles to send in the MDC1200 data stream.
+        /// </summary>
+        public int NumberOfPreambles
+        {
+            get { return encoder.NumberOfPreambles; }
+            set { encoder.NumberOfPreambles = value; }
+        }
+
+        /// <summary>
+        /// Flag indicating whether or not we should inject silence before the synthesized audio.
+        /// </summary>
+        public bool InjectSilenceLeader { get; set; }
+
+        /// <summary>
+        /// Flag indicating whether or not we should inject silence after the synthesized audio.
+        /// </summary>
+        public bool InjectSilenceTail { get; set; }
+
+        /// <summary>
+        /// Flag indicating whether or not we have synthesized samples queued.
+        /// </summary>
+        public bool HasSamples { get { return mdcGenerated; } }
+
+        /**
+         * Methods
+         */
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DPLGenerator"/> class.
+        /// </summary>
+        /// <param name="sampleRate"></param>
+        public MDCGenerator(int sampleRate)
+        {
+            waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, 1);
+            this.encoder = new MDCEncoder(sampleRate);
+            this.InjectSilenceLeader = false;
+            this.InjectSilenceTail = false;
+        }
+
+        /// <summary>
+        /// Synthesizes the given MDC packets.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="noPreamble"></param>
+        public void GenerateMDC(MDCPacket first, MDCPacket second = null, bool noPreamble = false)
+        {
+            if (first == null)
+                throw new ArgumentNullException();
+            if (second != null)
+            {
+                encoder.CreateDouble(first, second, noPreamble);
+                doublePacket = true;
+            }
+            else
+            {
+                encoder.CreateSingle(first, noPreamble);
+                doublePacket = false;
+            }
+            mdcGenerated = true;
+        }
+
+        /// <summary>
+        /// Gets a byte array of samples with the encoded MDC packet.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GetSamples()
+        {
+            if (!mdcGenerated)
+                return null;
+
+            int duration = 0;
+            if (doublePacket)
+                duration = (PACKET_LEN_MS * 2);
+            else
+                duration = PACKET_LEN_MS;
+            duration += (PREAMBLE_LEN_MS * NumberOfPreambles);
+
+            if (InjectSilenceLeader)
+                duration += SILENCE_MS;
+            if (InjectSilenceTail)
+                duration += SILENCE_MS;
+
+            SampleToAudioProvider16 smpTo16 = new SampleToAudioProvider16(this);
+
+            int sDuration = SamplesToMS.MSToSampleBytes(waveFormat, duration);
+            byte[] sigBuf = new byte[sDuration];
+            smpTo16.Read(sigBuf, 0, sDuration);
+
+            mdcGenerated = false;
+            return sigBuf;
+        }
+
+        /// <summary>
+        /// Reads from this provider.
+        /// </summary>
+        public int Read(float[] buffer, int offset, int count)
+        {
+            if (!mdcGenerated)
+                return 0;
+
+            int outIndex = offset;
+            int nSample = 0;
+
+            // read encoder bytes
+            byte[] mdcBuffer = encoder.GetSamples();
+            if (mdcBuffer != null)
+            {
+                if (InjectSilenceLeader)
+                {
+                    byte[] tmp = new byte[mdcBuffer.Length];
+                    Buffer.BlockCopy(mdcBuffer, 0, tmp, 0, mdcBuffer.Length);
+
+                    // generate silence
+                    int sDuration = SamplesToMS.MSToSampleBytes(WaveFormat, SILENCE_MS);
+                    mdcBuffer = new byte[tmp.Length + sDuration];
+
+                    for (int i = 0; i < sDuration; i++)
+                        mdcBuffer[i] = 128; // 128 should be audio "zero" in byte format
+
+                    // inject original MDC audio
+                    Buffer.BlockCopy(tmp, 0, mdcBuffer, sDuration, tmp.Length);
+                }
+
+                if (InjectSilenceTail)
+                {
+                    byte[] tmp = new byte[mdcBuffer.Length];
+                    Buffer.BlockCopy(mdcBuffer, 0, tmp, 0, mdcBuffer.Length);
+
+                    // generate silence
+                    int sDuration = SamplesToMS.MSToSampleBytes(WaveFormat, SILENCE_MS);
+                    mdcBuffer = new byte[tmp.Length + 1 + sDuration];
+
+                    // inject original MDC audio
+                    Buffer.BlockCopy(tmp, 0, mdcBuffer, 0, tmp.Length);
+
+                    // insert silence
+                    for (int i = tmp.Length + 1; i < (tmp.Length + 1 + sDuration); i++)
+                        mdcBuffer[i] = 128; // 128 should be audio "zero" in byte format
+                }
+
+                // complete buffer
+                for (int sampleCount = 0; sampleCount < count / waveFormat.Channels; sampleCount++)
+                {
+                    if (nSample >= mdcBuffer.Length)
+                        buffer[outIndex++] = 0;
+                    else
+                    {
+                        // convert 8-bit MDC to 16-bit
+                        buffer[outIndex++] = GAIN * (mdcBuffer[nSample] / 128f - 1.0f);
+                        nSample++;
+                    }
+                }
+                return count;
+            }
+            else
+                return 0;
+        }
+    } // public class MDCGenerator : ISampleProvider
+} // namespace libmdc
+#endif
---
+++ libmdc/libmdc/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * libmdc
+ * INTERNAL/PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Author: Bryan Biedenkapp <gatekeep@jmp.cx>
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("libmdc")]
+[assembly: AssemblyDescription("Library for encoding and decoding MDC1200 data bursts.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PLL Communications LLC")]
+[assembly: AssemblyProduct("libmdc")]
+[assembly: AssemblyCopyright("Copyright (c) PLL Communications LLC., 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aef4ea5b-fc10-4e7c-bae1-2f389983ec58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
